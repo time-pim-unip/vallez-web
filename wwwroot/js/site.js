58c2a7d5be690b04bb47c16be1d0c87dd8b2b893
DouglasSoaress <douglasvieirasoares@hotmail.com>
2021-10-28 21:58:46 -0300

    Atualizando dados

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -2,3 +2,32 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+/*
+function limpaOpcoesAbertas() {
+    let nodes = document.querySelectorAll(".barra .esquerda .link div");
+
+    nodes.forEach(n => n.classList.remove('is-active'));
+}
+*/
+
+document.querySelector("#inf-link").addEventListener('click', (e) => {
+    e.preventDefault();
+    if (document.querySelector("#contatos").classList.contains('is-active')) {
+        document.querySelector("#contatos").classList.remove('is-active');
+    }
+
+    document.querySelector("#informacao").classList.toggle("is-active");
+
+}); 
+
+
+document.querySelector("#cont-link").addEventListener('click', (e) => {
+    e.preventDefault();
+    if (document.querySelector("#informacao").classList.contains('is-active')) {
+        document.querySelector("#informacao").classList.remove('is-active');
+    }
+
+    document.querySelector("#contatos").classList.toggle("is-active");
+
+});
